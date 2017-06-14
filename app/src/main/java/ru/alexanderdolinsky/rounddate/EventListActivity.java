9453c8219d14f06f18ba98eaa7b7fca7ec2aa4f2
alexdolinsky <alexdolinsky@gmail.com>
2017-06-14 09:54:39 +0300

    Формирование активити EventList

--- app/src/main/java/ru/alexanderdolinsky/rounddate/EventListActivity.java
+++ app/src/main/java/ru/alexanderdolinsky/rounddate/EventListActivity.java
@@ -2,6 +2,7 @@ package ru.alexanderdolinsky.rounddate;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class EventListActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class EventListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_list);
+    }
+
+    public void onEditEventsGroup(View view) {
     }
 }
